a82dbf4e04f297fa3c52026c739b06832464c005
Reynard6307 <124555812+Reynard6307@users.noreply.github.com>
2025-02-17 18:01:24 +0800

    Upload ReAct

--- reyanrd_react.py
+++ reyanrd_react.py
@@ -6,7 +6,7 @@ from dsrag.document_parsing import extract_text_from_pdf
 # import libraries
 import openai
 import os
-from langchain.llms import OpenAI 
+from langchain_community.llms import OpenAI
 from langchain.agents import initialize_agent
 from langchain.tools import Tool
 # load API keys; you will need to obtain these if you haven't yet
@@ -19,110 +19,78 @@ load_dotenv()
 
 # Initialize OpenAI and KnowledgeBase
 llm = OpenAIChatAPI(model='gpt-4o-mini')
-reranker = NoReranker()
+reranker = CohereReranker()
 
-com_id = "CommAero"
-uss_id = "USS"
+# com_id = "CommAero"
+# uss_id = "USS"
 
-com_kb = KnowledgeBase(com_id, reranker=reranker, vector_db=ChromaDB(com_id), storage_directory="~/AI-Agents-For-ST/storage")
-uss_kb = KnowledgeBase(uss_id, reranker=reranker, vector_db=ChromaDB(uss_id), storage_directory="~/AI-Agents-For-ST/storage")
+# com_kb = KnowledgeBase(com_id, reranker=reranker, vector_db=ChromaDB(com_id), storage_directory="~/AI-Agents-For-ST/storage")
+# uss_kb = KnowledgeBase(uss_id, reranker=reranker, vector_db=ChromaDB(uss_id), storage_directory="~/AI-Agents-For-ST/storage")
 
-def query_kb(kb, query):
-    document = kb.query(query)
+# Assuming KnowledgeBase already exist
+def query_kb(sector_id, query, reranker):
+    sector_kb = KnowledgeBase(sector_id, reranker=reranker, vector_db=ChromaDB(sector_id), storage_directory="~/AI-Agents-For-ST/storage")
+    document = sector_kb.query(query)
     return document[0]["text"] if document else "No relevant information found."
 
 # def query_uss_kb(query):
 #     document = uss_kb.query(query)
 #     return document[0]["text"] if document else "No relevant information found."
 
-tools = [{
-    "type": "function",
-    "function": {
-        "name": "query_com_kb",
-        "description": "Query a knowledge base to retrieve relevant info for companies related to Commercial Aerospace.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "query": {
-                    "type": "string",
-                    "description": "User's question about Commercial Aerospace companies."
-                },
-            },
-            "required": ["query"]
-        }
-    }
-}, {
-    "type": "function",
-    "function": {
-        "name": "query_uss_kb",
-        "description": "Query a knowledge base to retrieve relevant info for Universal Satellite Services.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "query": {
-                    "type": "string",
-                    "description": "User's question about Universal Satellite Services companies."
-                },
-            },
-            "required": ["query"]
-        }
-    }
-}]
+# ReAct Automation
 
+path = "./storage/metadata"   
+files_list = os.listdir(path)
 
+sector_ids = []
+
+for i in range(len(files_list)):
+    sector_id = os.listdir(path)[i][:-5]
+    sector_ids.append(sector_id)
 
 client = OpenAI()
 
-# ReAct tryout
-my_tool1= Tool(
-    name="query_kb",
-    func= query_kb,
-    description="Query a knowledge base to retrieve relevant info for companies related to {name}."
-)
+question = "What is the revenue of AerSale Coorp 2022 and Satcom Cometech 2021"
 
-my_tool2= Tool(
-    name="query_uss_kb",
-    func= query_uss_kb,
-    description="Query a knowledge base to retrieve relevant info for companies related to Universal Satellite Services."
-)
-
-
-
-tools = [my_tool1, my_tool2]
+tools = [
+    Tool(
+        name=f"Tool{i+1}",
+        func=query_kb(sector_id=sector_ids[i], query=question, reranker=reranker),
+        description=f"Query a knowledge base to retrieve relevant info for companies related to {sector_ids[i]}."
+    )
+    for i in range(len(files_list))
+]
 
 agent = initialize_agent(tools,client, agent="zero-shot-react-description", verbose=True)
-
-question = "What is the revenue of Airbus 2022 and Echostar 2021"
-
-prompt = """
-You are a financial analyst at a large investment firm. You have been asked to analyze the financial performance of a company.
-
-You run in a loop of Thought, Suggestions, Action, PAUSE, Observation.
-At the end of the loop, you output an Answer.
-Use Thought to describe your thoughts about the question you have been asked.
-Use Action to run one of the suitable actions available to you - then return PAUSE.
-Observation will be the result of running those actions.
-
-Your available actions are:
-
-query_com_kb:
-- Example: query_com_kb: What is the revenue of SpiritAeroSystems 2022
-- Retrieves information about companies in the Commercial Aerospace Industry.
-
-query_uss_kb:
-- Example: query_uss_kb: What is the revenue of Echostar 2021
-- Retrieves information about companies in the Universal Satellite Services Industry.
-
-Example session:
-
-Question: What is the revenue of SpiritAeroSystems in 2022 and General Echostar in 2021
-Thought: I need to find the revenue of SpiritAeroSystems 2022 (Commercial Aerospace) and Echostar 2021 (Universal Satellite Services).
-
-Action: query_com_kb: SpiritAeroSystems 2022
-PAUSE
-"""
-
 agent.run(question)
+
+# prompt = """
+# You are a financial analyst at a large investment firm. You have been asked to analyze the financial performance of a company.
+
+# You run in a loop of Thought, Suggestions, Action, PAUSE, Observation.
+# At the end of the loop, you output an Answer.
+# Use Thought to describe your thoughts about the question you have been asked.
+# Use Action to run one of the suitable actions available to you - then return PAUSE.
+# Observation will be the result of running those actions.
+
+# Your available actions are:
+
+# query_com_kb:
+# - Example: query_com_kb: What is the revenue of SpiritAeroSystems 2022
+# - Retrieves information about companies in the Commercial Aerospace Industry.
+
+# query_uss_kb:
+# - Example: query_uss_kb: What is the revenue of Echostar 2021
+# - Retrieves information about companies in the Universal Satellite Services Industry.
+
+# Example session:
+
+# Question: What is the revenue of SpiritAeroSystems in 2022 and General Echostar in 2021
+# Thought: I need to find the revenue of SpiritAeroSystems 2022 (Commercial Aerospace) and Echostar 2021 (Universal Satellite Services).
+
+# Action: query_com_kb: SpiritAeroSystems 2022
+# PAUSE
+# """
 
 # Start conversation
 # messages = [{"role": "system", "content": prompt}]
