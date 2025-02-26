4cfd5bc20bd3f2366b585767146881b5568561e3
Yongqing929 <limyongqing74@gmail.com>
2025-02-26 13:28:34 +0800

    Update neo4j_tools.py

--- neo4j_tools.py
+++ neo4j_tools.py
@@ -3,6 +3,8 @@ from langchain.chains import LLMChain
 from langchain.graphs import Neo4jGraph
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
