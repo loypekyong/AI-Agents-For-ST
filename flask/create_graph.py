2a1dac58d7045b36bc2773451b7b9ea684b4c701
Yongqing929 <limyongqing74@gmail.com>
2025-04-02 12:12:32 +0800

    added graph functions to flask

---
+++ flask/create_graph.py
@@ -0,0 +1,235 @@
+import sys
+import os
+import shutil
+sys.path.append('C:/Users/limyo/anaconda3/envs/dsrag/Lib/site-packages/neo4j')
+from neo4j import GraphDatabase
+import json
+from collections import defaultdict
+from dotenv import load_dotenv
+load_dotenv()
+
+class Neo4jConnection:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        try:
+            self.driver.verify_connectivity()
+            print("Successfully connected to Neo4j")
+        except Exception as e:
+            print("Connection failed:", e)
+
+    def close(self):
+        self.driver.close()
+
+    def load_json_files_from_directory(self, directory):
+        grouped_data = defaultdict(list)
+    
+        # Traverse the directory recursively
+        for dirpath, _, filenames in os.walk(directory):
+            for filename in filenames:
+                if filename.endswith('.json'):
+                    file_path = os.path.join(dirpath, filename)
+                    with open(file_path, 'r') as f:
+                        try:
+                            # Load the data from the JSON file
+                            data = json.load(f)
+                            # Check if data is a list
+                            if isinstance(data, dict):
+                                for kb_id, entry in data.items():
+                                    for ind in range(len(entry)):
+                                        if 'doc_id' in entry[ind]:
+                                            # print("key: ", entry[ind]["doc_id"])
+                                            key = entry[ind]["doc_id"]
+                                            grouped_data[key].append(entry[ind])
+                                        else:
+                                            print(f"Warning: 'kb_id' not found in entry: {entry}")
+                            else:
+                                # print(f"Warning: Expected a dict, got {type(data)} instead.")
+                                print()
+
+                        except json.JSONDecodeError as e:
+                            print(f"Error decoding JSON from {file_path}: {e}")
+
+        return grouped_data
+    
+    def create_data(self, session, data):
+        with session.begin_transaction() as tx:
+            for doc_id, entries in data.items():
+                document_title = entries[0]['document_title']
+                document_summary = entries[0]['document_summary']
+                doc_year = doc_id.split(".")[0][-4:]
+                kb_id = entries[0]["kb_id"]
+                doc_id_sections = doc_id.split("_")
+                print(doc_id_sections)
+                print(doc_year)
+                print("--------------")
+
+
+                root_query = f'''
+                MERGE (r:Root {{
+                    root_id: $root_id
+                }}) 
+                ON CREATE SET r.root_id=$root_id, r.kb_id=$kb_id
+                '''
+                tx.run(root_query, root_id=doc_id_sections[0], kb_id=kb_id)
+                
+
+                ########################################################################
+                
+
+                sector_query = f'''
+                MERGE (sc:Sector {{
+                    sector_id: $sector_id
+                }}) 
+                ON CREATE SET sc.sector_id=$sector_id, sc.kb_id=$kb_id
+                '''
+                tx.run(sector_query, sector_id=doc_id_sections[1], kb_id=kb_id)
+
+
+                relationship_query = f'''
+                MATCH (r:Root {{root_id: $root_id}}),
+                    (sc:Sector {{sector_id: $sector_id}}) 
+                MERGE (r)-[:HAS_SECTOR]->(sc)
+                '''
+
+                # tx.run(relationship_query, kb_id=kb_id, sector_id=doc_id_sections[1])
+
+                tx.run(relationship_query, root_id=doc_id_sections[0], sector_id=doc_id_sections[1])
+
+                ########################################################################
+
+
+                department_query = f'''
+                MERGE (dp:Department {{
+                    department_id: $department_id
+                }}) 
+                ON CREATE SET dp.department_id = $department_id, dp.kb_id=$kb_id
+                '''
+                tx.run(department_query, department_id=doc_id_sections[2], kb_id=kb_id)
+
+
+                relationship_query = f'''
+                MATCH (sc:Sector {{sector_id: $sector_id}}),
+                    (dp:Department {{department_id: $department_id}}) 
+                MERGE (sc)-[:HAS_DEPARTMENT]->(dp)
+                '''
+                tx.run(relationship_query, sector_id=doc_id_sections[1], department_id=doc_id_sections[2])
+            
+
+                # #########################################################################
+
+
+                year_query = f'''
+                MERGE (y:Year {{
+                    doc_year: $doc_year
+                }}) 
+                ON CREATE SET y.doc_year=$doc_year
+                '''
+                tx.run(year_query, doc_year=doc_year)
+        
+                relationship_query = f'''
+                MATCH (dp:Department {{department_id: $department_id}}),
+                    (y:Year {{doc_year: $doc_year}}) 
+                MERGE (dp)-[:IN_YEAR]->(y)
+                '''
+                tx.run(relationship_query, department_id=doc_id_sections[2], doc_year=doc_year)
+            
+
+                # #########################################################################
+
+            
+                doc_title = f'''
+                MERGE (dt:Document_title {{
+                    doc_id: $doc_id 
+                }}) 
+                ON CREATE SET dt.doc_year=$doc_year, dt.doc_title=$doc_title
+                '''
+                tx.run(doc_title, doc_title=document_title, doc_year=doc_year, doc_id=doc_id.split(".")[0][:-4]+doc_year)
+
+                relationship_query = f'''
+                MATCH (y:Year {{doc_year: $doc_year}}),
+                    (dt:Document_title {{doc_title: $doc_title, doc_id: $doc_id}}) 
+                MERGE (y)-[:COVERS]->(dt)
+                '''
+                tx.run(relationship_query, doc_year=doc_year, doc_title=document_title, doc_id=doc_id.split(".")[0][:-4]+doc_year)
+
+            
+
+                # #########################################################################
+
+
+                doc_summ = f'''
+                MERGE (ds:Document_summ {{
+                    doc_summary: $doc_summary
+                }}) 
+                ON CREATE SET ds.doc_title=$doc_title
+                '''
+                tx.run(doc_summ, doc_summary=document_summary, doc_title=document_title)
+
+                relationship_query = f'''
+                MATCH (dt:Document_title {{doc_title: $doc_title}}),
+                    (ds:Document_summ {{doc_summary: $doc_summary}}) 
+                MERGE (dt)-[:HAS_SUMMARY]->(ds)
+                '''
+                tx.run(relationship_query, doc_title=document_title, doc_summary=document_summary)
+            
+
+                # ########################################################################
+
+                section_source = kb_id + ":" + doc_id_sections[1] + ":" + doc_id_sections[2] + ":" + doc_year
+                for entry in entries:
+                    # Create Section Node
+                    sec_title = entry["section_title"]
+                    sec_chunks = entry["chunk_text"]
+                    
+                    if sec_title is None:
+                        sec_title = ""
+                    
+                    # Use a combination of sec_title and section_source to ensure uniqueness
+                    section_title = f'''
+                    MERGE (s:Section {{
+                        sec_title: $sec_title,
+                        section_source: $section_source
+                    }})
+                    ON CREATE SET s.sec_title=$sec_title, s.sec_chunks=$sec_chunks, s.section_source=$section_source
+                    '''
+                    tx.run(section_title, sec_title=sec_title, sec_chunks=sec_chunks, section_source=section_source)
+
+                    # Create Relationship between Document and Section
+                    relationship_query = f'''
+                    MATCH (ds:Document_summ {{doc_summary: $doc_summary}}), 
+                        (s:Section {{sec_title: $sec_title, section_source: $section_source}})
+                    MERGE (ds)-[:HAS_SECTION_TITLE]->(s)
+                    '''
+                    tx.run(relationship_query, sec_title=sec_title, doc_summary=document_summary, section_source=section_source)
+
+
+                #     ########################################################################
+
+
+if __name__ == "__main__":
+    uri = os.getenv("NEO4J_URI")
+    user = os.getenv("NEO4J_USERNAME")
+    password = os.getenv("NEO4J_PASSWORD")  
+
+    connection = Neo4jConnection(uri, user, password)
+
+    directory_path = 'data_new/'  # Source directory
+    completed_directory = 'data_completed/'  # Destination directory
+    data = connection.load_json_files_from_directory(directory_path)
+
+    try:
+        with connection.driver.session() as session:
+            connection.create_data(session, data)
+            print("Done")
+
+            # Move JSON files to completed directory
+            for filename in os.listdir(directory_path):
+                if filename.endswith('.json'):  # Ensure you're moving only JSON files
+                    src_path = os.path.join(directory_path, filename)
+                    dst_path = os.path.join(completed_directory, filename)
+                    shutil.move(src_path, dst_path)
+                    print(f'Moved: {src_path} to {dst_path}')
+    except Exception as e:
+        print("Error occurred during data creation:", e)
+
+    connection.close()
