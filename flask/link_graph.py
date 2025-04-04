f6fb7121d2556d483d6a9b46d9bae154b1e89ddf
Yongqing929 <limyongqing74@gmail.com>
2025-04-04 13:16:00 +0800

    fix: removed redundant files and minor fix to link graph

--- flask/link_graph.py
+++ flask/link_graph.py
@@ -32,18 +32,42 @@ class Neo4jConnection:
         CALL apoc.periodic.iterate(
             "MATCH (n:Section) WHERE n.sec_title IS NOT NULL RETURN n",
             "WITH n
-            MATCH (m:Section) 
+            MATCH (m:Section)
             WITH n, m, apoc.text.sorensenDiceSimilarity(m.sec_title, n.sec_title) AS similarity
             WHERE similarity >= 0.7
+            AND NOT ((m)-[:IS_RELATED_TO]->(n) OR (n)-[:IS_RELATED_TO]->(m))
             CREATE (m)-[:IS_RELATED_TO {similarity: similarity}]->(n)",
             {batchSize: 100, parallel: False}
-        )
+            )
         """)
 
         session.run("""
         MATCH (a:Section)-[rel:IS_RELATED_TO]->(a) 
         DELETE rel;            
         """)
+
+        # To unlink graphs
+        # def link_sections(self, session):
+        # # # Iterate over all pairs of nodes
+        # session.run("""
+        # CALL apoc.periodic.iterate(
+        #     "MATCH (n:Section) WHERE n.sec_title IS NOT NULL RETURN n",
+        #     "WITH n
+        #     MATCH (m:Section) 
+        #     WITH n, m, apoc.text.sorensenDiceSimilarity(m.sec_title, n.sec_title) AS similarity
+        #     WHERE similarity >= 0.7
+        #     MERGE (m)-[:IS_RELATED_TO {similarity: similarity}]->(n)
+        #     ON CREATE SET rel.similarity = similarity
+        #     ON MATCH SET rel.similarity = GREATEST(rel.similarity, similarity)",
+        #     {batchSize: 100, parallel: False}
+        # )
+        # """)
+
+        # session.run("""
+        # MATCH (a:Section)-[rel:IS_RELATED_TO]->(b:Section)
+        # WHERE a <> b
+        # DELETE rel;            
+        # """)
 
 if __name__ == "__main__":
     uri = os.getenv("NEO4J_URI")
