6a1368db50d3eef28ac225d12e67f53e4180d5ca
Yongqing929 <limyongqing74@gmail.com>
2025-04-04 13:18:28 +0800

    Update neo4j_tools

--- flask/neo4j_tools.py
+++ flask/neo4j_tools.py
@@ -125,5 +125,8 @@ def query_neo4j(graph, llm, query):
         if section_source not in grouped_sections:
             grouped_sections[section_source] = []
         grouped_sections[section_source].append(primary_section)
-        res = grouped_sections
-    return res
+    
+    if grouped_sections == {}:
+        return res
+    else:
+        return grouped_sections
